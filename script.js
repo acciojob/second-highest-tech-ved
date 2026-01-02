7cd6647048f0ee3ce056fdba7052bcc2a8a7aa4a
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-01-02 12:50:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+function secondHighest(arr) {
+    if (arr.length < 2) return -Infinity;
+
+    let max = -Infinity;
+    let secondMax = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            if (max !== arr[i]) {
+                secondMax = max;
+            }
+            max = arr[i];
+        } else if (arr[i] < max && arr[i] > secondMax) {
+            secondMax = arr[i];
+        }
+    }
+
+    return secondMax;
+}
+
